554a656934c2d34bc860e1156feff8a00d527a67
zhorton <zak.horton@vml.com>
2019-11-06 17:47:05 -0600

    Reset project to follow gif tutorial instead checking out branches

--- src/components/basic-form.vue
+++ src/components/basic-form.vue
@@ -1,27 +1,9 @@
 <template>
-  <form class='form'> 
-    <h1> {{ title | capitalize }} </h1>
-
-    <input type='text' name='name' placeholder="Name" />
-    <input type='email' name='email' placeholder="Email" />
-    <input type='text' name='phone_number' placeholder="Phone Number"/>
-
-    <button type='submit'>
-      Save {{ title }}
-    </button>
+  <form class='form'>
   </form>
 </template>
 
 <script>
 export default {
-  name: 'basic-form',
-  data() {
-    return {
-      title: 'Basic Form',
-      name: null,
-      email: null,
-      phone_number: null
-    }
-  },
 }
 </script>
