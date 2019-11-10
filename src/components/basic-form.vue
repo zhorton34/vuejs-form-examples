07a5b28c16e2c3b83819c6f1af0043cd7cf4db47
zhorton <zak.horton@vml.com>
2019-11-09 22:11:37 -0600

    Tutorial Starting Point

--- src/components/basic-form.vue
+++ src/components/basic-form.vue
@@ -1,5 +1,24 @@
 <template>
   <form class='form'>
+  	 <h1>Basic Form</h1>
+  	 <input 
+  	 	type='text' 
+  	 	name='name' 
+  	 	placeholder='Name' 
+	 />
+  	 <input 
+  	 	type='email' 
+  	 	name='email' 
+  	 	placeholder='Email' 
+	 />
+  	 <input 
+  	 	type='text' 
+  	 	name='city' 
+  	 	placeholder='City Location' 
+  	 />
+  	 <button>
+  	 	Save
+  	 </button>
   </form>
 </template>
 
