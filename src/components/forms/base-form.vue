79cd49e0f457511053acd105e9316d44ec3e8793
zhorton <zak.horton@vml.com>
2019-11-12 20:02:32 -0600

    Updated starting point

--- src/components/forms/base-form.vue
+++ src/components/forms/base-form.vue
@@ -1,30 +1,36 @@
 <template>
-
-<form class='form'>
-    <h1>Base Form</h1>
-    <input 
-        type='text'
-        name='name'
-        placeholder='Name'
-    />
-    <input 
-        type='email'
-        name='email'
-        placeholder='Email'
-    />
-    <input 
-        type='text'
-        name='city'
-        placeholder='City Location'
-    />
-    <button>
-        Save Base Form
-    </button>
-</form>
+    <form class='form'>
+        <h1>Base Form</h1>
+        <input v-model='name'
+            type='text'
+            name='name'
+            placeholder='Name'
+        />
+        <input v-model='city'
+            type='text'
+            name='city'
+            placeholder='City Location'
+        />
+        <input v-model='email'
+            type='email'
+            name='email'
+            placeholder='Email'
+        />
+        <button>
+            Save Base Form
+        </button>
+    </form>
 </template>
 
 <script>
     export default {
-        name: 'base-form'
+        name: 'base-form',
+        data() {
+            return {
+                name: null,
+                city: null,
+                email: null,
+            }
+        }
     }
 </script>
